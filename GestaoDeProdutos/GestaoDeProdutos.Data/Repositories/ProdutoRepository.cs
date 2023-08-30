6e4efe389486b69f99b3edaa7bbe596165759fdb
Joao Cesar <127664429+joaocesarr@users.noreply.github.com>
2023-08-30 20:14:02 -0300

    Add files via upload

---
+++ GestaoDeProdutos/GestaoDeProdutos.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using GestaoDeProdutos.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeProdutos.Data.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+
+    {
+    }
+}
